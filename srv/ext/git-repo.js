9213cba29fb5d4ace25bd56d4ec2e7f423e08b19
Da Chen <da.chen@me.com>
2023-11-13 21:31:01 -0800

    Update landscape content

--- srv/ext/git-repo.js
+++ srv/ext/git-repo.js
@@ -30,8 +30,8 @@ class GitRepo {
     }
 
     commitAndPush( message, userName, userEmail ) {
-        let ret1 = execSync(`cd ${LOCAL_REPO_PATH} && git config user.name "${userName}" && git config user.email "${userEmail}" && git config push.autoSetupRemote true`).toString();
-        let ret2 = execSync(`git add -A && git commit -m '${message}' && git push --force`).toString();
+        let ret1 = execSync(`cd ${LOCAL_REPO_PATH} && git config user.name "${userName}" && git config user.email "${userEmail}"`).toString();
+        let ret2 = execSync(`git add -A && git commit -m '${message}' && git push --set-upstream origin ${GIT_REPO_BRANCH} --force`).toString();
         console.log(`commit and push completed, ret1=\n${ret1}\nret2=\n${ret2}`);
     }
 }
